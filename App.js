7864d16d71b8112d035827f435c8c38890a6e520
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-06-28 20:19:39 +0300

    created all the screens nedded

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native' 
+import Home from './Screens/HomePage';
+import Favorites from './Screens/FavoritesPage';
+import  Settings from './Screens/SettingsPage';
 
 export default function App() {
+  const Tabs = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator>
+        <Tabs.Screen name='Home' component={Home}/> 
+        <Tabs.Screen name='Favorites' component={Favorites}/> 
+        <Tabs.Screen name='Settings' component={Settings}/> 
+      </Tabs.Navigator>
+    </NavigationContainer>
+
   );
+
 }
 
 const styles = StyleSheet.create({
---
+++ Screens/HomePage.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default function HomePage(){
+
+    return(
+        <View>
+            <Text>HomePage</Text>
+        </View>
+    )
+}
