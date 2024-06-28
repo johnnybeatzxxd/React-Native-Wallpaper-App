8e991954def4ac15273665f45a00114150841216
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-06-28 21:18:08 +0300

    added icons on the tab bar

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import {  StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { CardStyleInterpolators } from '@react-navigation/stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {NavigationContainer} from '@react-navigation/native' 
 import Home from './Screens/HomePage';
 import Favorites from './Screens/FavoritesPage';
@@ -12,9 +14,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tabs.Navigator>
-        <Tabs.Screen name='Home' component={Home}/> 
-        <Tabs.Screen name='Favorites' component={Favorites}/> 
-        <Tabs.Screen name='Settings' component={Settings}/> 
+        <Tabs.Screen name='Home' component={Home} options={{tabBarIcon:({focused,color,size})=>(<MaterialCommunityIcons name="home" color={color} size={size}/>)}}/> 
+        <Tabs.Screen name='Favorites' component={Favorites} options={{tabBarIcon:({focused,color,size})=>(<MaterialCommunityIcons name="heart" color={color} size={size}/>)}}/> 
+        <Tabs.Screen name='Settings' component={Settings} options={{tabBarIcon:({focused,color,size})=>(<MaterialCommunityIcons name="cog" color={color} size={size}/>)}}/> 
       </Tabs.Navigator>
     </NavigationContainer>
 
